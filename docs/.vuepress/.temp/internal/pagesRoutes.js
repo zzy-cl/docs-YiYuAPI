9d6cc9ea7dca0968006400b8615fa15ac88b0624
zzy-cl <1580574280@qq.com>
2023-05-21 00:43:19 +0800

    test3

--- docs/.vuepress/.temp/internal/pagesRoutes.js
+++
@@ -1,7 +0,0 @@
-export const pagesRoutes = [
-  ["v-8daa1a0e","/",{"title":""},["/index.html","/index.md"]],
-  ["v-eb14d09e","/guide/changelog.html",{"title":"更新日志"},["/guide/changelog","/guide/changelog.md"]],
-  ["v-fffb8e28","/guide/",{"title":"介绍"},["/guide/index.html","/guide/index.md"]],
-  ["v-067f8a78","/guide/rewm.html",{"title":"视频水印"},["/guide/rewm","/guide/rewm.md"]],
-  ["v-3706649a","/404.html",{"title":""},["/404"]],
-]
--- docs/.vuepress/.temp/pages/guide/changelog.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><h1 id="更新日志" tabindex="-1"><a class="header-anchor" href="#更新日志" aria-hidden="true">#</a> 更新日志</h1>
-<h2 id="_2023-05-20" tabindex="-1"><a class="header-anchor" href="#_2023-05-20" aria-hidden="true">#</a> 2023.05.20</h2>
-<p>项目和项目文档搭建完成。</p>
-<p>添加<strong>视频去水印接口</strong>。</p>
-<p>添加<em><strong>抖音视频去水印功能</strong></em>和<em><strong>快手去水印功能</strong></em>。</p>
-</div></template>
-
-
